7c6578a54593bdfaf58603bb47851d58e087a517
Algozx <ra-1944630-5@alunos.unicesumar.edu.br>
2021-01-25 01:49:10 -0300

    Adicionando Warp
    Adicionando warp entre os portais !

---
+++ Assets/Scripts/SceneManagement/Fader.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class Fader : MonoBehaviour
+    {
+        CanvasGroup canvas;
+
+        void Start()
+        {
+            canvas = GetComponent<CanvasGroup>();
+        }
+
+        IEnumerator FadeInOut()
+        {
+            yield return FadeOut(3f);
+            print("Faded out");
+            yield return FadeIn(2f);
+        }
+
+        public IEnumerator FadeOut(float timeF)
+        {
+            while (canvas.alpha < 1)
+            {
+                canvas.alpha += Time.deltaTime / timeF;
+                yield return null;
+            }
+        }
+        public IEnumerator FadeIn(float timeF)
+        {
+            while (canvas.alpha > 0)
+            {
+                canvas.alpha -= Time.deltaTime / timeF;
+                yield return null;
+            }
+        }
+    }
+}
